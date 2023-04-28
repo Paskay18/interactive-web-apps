81d25c6e5d01544da0bb22ffd761700ea391dade
Paskay18 <124031153+Paskay18@users.noreply.github.com>
2023-04-28 23:20:55 +0200

    IWA 13 solutions
    -added parseInt
    -added return to both functions
    -added = to the >=
    -John(3) final result

---
+++ IWA13/script.js
@@ -0,0 +1,27 @@
+let state = 'idle'
+let user = null
+let calculated = '1'
+
+// Only allowed to change below
+
+
+const logCalc = () => { 
+    const isString = typeof calculated === 'numerical-string';
+    const calculatedAsNumber = isString ? calculated: parseInt(calculated);  //used parseNumber not parseInt, 
+    calculated =calculatedAsNumber + 2;        //used === not =, had 1 instead of 2
+    return calculated;  //didnt return value
+}
+
+
+const  calcUser= () => {                                  //had logcalc
+  if (calculated >= 2) {user = 'John'};                     
+  if (calculated >= 2) {state = 'requesting'};
+  if (calculated >= 3) {state = 'idle'};
+  return calculated;
+}
+
+const checkUser = () => {
+	if ( user && state !== 'requesting') {                 
+		console.log(`User: ${user} (${calculated})`)
+	}
+}
