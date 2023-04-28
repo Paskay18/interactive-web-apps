03aab31d2ddc216aaa6835530e015052dedbaa63
Paskay18 <124031153+Paskay18@users.noreply.github.com>
2023-04-28 04:01:04 +0200

    IWA12 solutions
    ]-Objects created
    -style element added

--- interactive-web-apps/IWA12/script.js
+++ interactive-web-apps/IWA12/script.js
@@ -33,33 +33,33 @@ const STATUS_MAP = {
  
 
 const book1 ={
-    status1: document.querySelector('#book1.status'),
-    reserve1 : document.querySelector('#book1.reserve'),
-    checkOut1: document.querySelector('#book1.checkout'),
-    checkIn1: document.querySelector('#book1.checkin'),
+    status1: document.getElementById('#book1.status'),
+    reserve1 : document.getElementById('#book1.reserve'),
+    checkOut1: document.getElementById('#book1.checkout'),
+    checkIn1: document.getElementById('#book1.checkin'),
 }
 
 const book2 ={
-    status1: document.querySelector('#book2.status'),
-    reserve1 : document.querySelector('#book2.reserve'),
-    checkOut1: document.querySelector('#book2.checkout'),
-    checkIn1: document.querySelector('#book2.checkin'),
+    status1: document.getElementById('#book2.status'),
+    reserve1 : document.getElementById('#book2.reserve'),
+    checkOut1: document.getElementById('#book2.checkout'),
+    checkIn1: document.getElementById('#book2.checkin'),
 }
 
 const book3 ={
-    status1: document.querySelector('#book3.status'),
-    reserve1 : document.querySelector('#book3.reserve'),
-    checkOut1: document.querySelector('#book3.checkout'),
-    checkIn1: document.querySelector('#book3.checkin'),
+    status1: document.getElementById('#book3.status'),
+    reserve1 : document.getElementById('#book3.reserve'),
+    checkOut1: document.getElementById('#book3.checkout'),
+    checkIn1: document.getElementById('#book3.checkin'),
 }
 
 //book1 Overdue
 
-
+book1.checkIn1.style.filter = 'yellow',
 book1.status1.style.color= STATUS_MAP.overdue.color;
 book1.reserve1.disabled =! STATUS_MAP.overdue.canReserve;
 book1.checkOut1.disabled=! STATUS_MAP.overdue.canCheckout;
-book1.checkIn1.disabled=! STATUS_MAP.overdue.canCheckIn;
+book1.checkIn1.disabled=== STATUS_MAP.overdue.canCheckIn;
 
 
 
